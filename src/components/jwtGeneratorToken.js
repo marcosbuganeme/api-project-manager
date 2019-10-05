6b696a9d65e5ea06ff5ffc84a810ea7a57a8767a
Marcos Olavo <marcos.after@gmail.com>
2019-10-05 19:59:17 -0300

    adicionando componente para geracao de token

---
+++ src/components/jwtGeneratorToken.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken')
+
+const authConfig = require('../config/jwt/auth.json')
+
+const JwtGenerateToken = function gerarToken(parametros = {}) {
+
+    return jwt.sign(parametros, authConfig.secret, {
+        expiresIn: 86400
+    })
+}
+
+module.exports = JwtGenerateToken
