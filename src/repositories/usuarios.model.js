26bca73d4c1ada3ab5d42e2de1d38b96c341ce95
Marcos Olavo <marcos.after@gmail.com>
2019-10-05 20:04:17 -0300

    estrutura da coleçao de usuario na api utilizando mongodb

---
+++ src/repositories/usuarios.model.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcryptjs')
+
+const mongoose = require('../config/banco-dados/index')
+
+const mongo = mongoose.module
+
+const UsuarioSchema = new mongo.Schema({
+    email: { 
+        type: String, 
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    senha: { 
+        type: String, 
+        required: true,
+        select: false
+    },
+    dataCriacao: { 
+        type: Date, 
+        required: true,
+        default: Date.now
+    },
+})
+
+UsuarioSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.senha, 10)
+    this.senha = hash
+
+    return next()
+})
+
+const UsuarioRepository = mongo.model('usuarios', UsuarioSchema)
+
+module.exports = UsuarioRepository
