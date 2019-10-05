6d34da355055b3b567dd3b2d72b094c095e05241
Marcos Olavo <marcos.after@gmail.com>
2019-10-05 20:44:03 -0300

    adicionando configuraçao do nodemailer

---
+++ src/config/enviar-email/mailer.js
@@ -0,0 +1,14 @@
+const nodemailer = require('nodemailer')
+
+const { host, port, user, pass } = require('./mail.json')
+
+const transport = nodemailer.createTransport({
+    host,
+    port,
+    auth: {
+      user, 
+      pass
+    }
+  });
+
+  module.exports = transport
