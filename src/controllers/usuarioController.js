c2d5484a9acf63cf55d811877f4c93ebe408962f
Marcos Olavo <marcos.after@gmail.com>
2019-10-05 20:08:51 -0300

    formatando codigo e adicionando novas caracteristicas no package.json

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,6 +1,6 @@
 const express = require('express')
-
 const authMiddleware = require('../middleware/auth')
+
 const UsuarioRepository = require('../repositories/usuarios.model')
 const JwtGenerateToken = require('../components/jwtGeneratorToken')
 
