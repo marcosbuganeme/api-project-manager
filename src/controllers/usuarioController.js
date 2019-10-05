c80f3c9e84798734658390ca7238e836cf83ce32
Marcos Olavo <marcos.after@gmail.com>
2019-10-05 20:02:34 -0300

    rotas para cadastrado e obtencao de cadastros {requer autenticacao para recuperar os dados}

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,50 @@
+const express = require('express')
+
+const authMiddleware = require('../middleware/auth')
+const UsuarioRepository = require('../repositories/usuarios.model')
+const JwtGenerateToken = require('../components/jwtGeneratorToken')
+
+const router = express.Router()
+
+router.get('/sincronizar', authMiddleware, async (req, res) => {
+
+    const usuarios = await UsuarioRepository.find()
+    return res.status(200).send(usuarios)
+})
+
+router.post('/cadastrar', async (req, res) => {
+
+    const { email } = req.body
+
+    try {
+
+        if ( await UsuarioRepository.findOne({ email })) {
+
+            return res.status(400).send({
+                error: {
+                    resume: 'E-mail duplicado',
+                    detail: `E-mail [${email}] já foi cadastrado, utilize outro`
+                }
+            })
+        }
+
+        const usuario = await UsuarioRepository.create(req.body)
+        usuario.senha = undefined
+
+        return res.send({ 
+            usuario, 
+            token: JwtGenerateToken({ id: usuario.id }) 
+        })
+
+    } catch (error) {
+
+        return res.status(400).send({
+            error: {
+                resume: 'Erro no registro',
+                detail: 'Informe todos os dados obrigatório antes de salvar um usuário'
+            }
+        })
+    }
+})
+
+module.exports = app => app.use('/clientes', router)
