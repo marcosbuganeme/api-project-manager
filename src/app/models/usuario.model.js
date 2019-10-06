85840414d5b96af766aecb38a1b887145b418d03
Marcos Olavo <marcos.after@gmail.com>
2019-10-06 17:39:57 -0300

    corrigindo modelagem de usuario

--- src/app/models/usuario.model.js
+++ src/app/models/usuario.model.js
@@ -43,6 +43,6 @@ UsuarioSchema.pre('save', async function(next) {
     return next()
 })
 
-const UsuarioRepositoryeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkOThlODBmMGI5ZDg5MGIwZjk3Yjc0NCIsImlhdCI6MTU3MDM5MzY2NSwiZXhwIjoxNTcwNDgwMDY1fQ.hsXnK7bkPHP0Bg7yukj2B1pKE8UET4vBHFrQWbPTmxg = mongo.model('Usuario', UsuarioSchema)
+const UsuarioRepository = mongo.model('Usuario', UsuarioSchema)
 
 module.exports = UsuarioRepository
