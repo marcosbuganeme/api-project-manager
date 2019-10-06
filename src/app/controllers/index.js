237edc68552b94fe3999050aa4ded037643de766
Marcos Olavo <marcos.after@gmail.com>
2019-10-06 16:26:48 -0300

    melhorando visual de mensagem para confirmação de recebimento da ação de troca de senha

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -4,6 +4,6 @@ const path = require('path')
 module.exports = app => {
     fs
     .readdirSync(__dirname)
-    .filter(arquivo => ((arquivo.indexOf('.')) !== 0 && (arquivo !== 'index.js')))
-    .forEach(arquivo => require(path.resolve(__dirname, arquivo))(app))
+    .filter(arquivoDoProjeto => ((arquivoDoProjeto.indexOf('.')) !== 0 && (arquivoDoProjeto !== 'index.js')))
+    .forEach(arquivoDoProjeto => require(path.resolve(__dirname, arquivoDoProjeto))(app))
 }
